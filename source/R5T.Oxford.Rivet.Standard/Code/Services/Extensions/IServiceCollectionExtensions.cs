b5b7f61108a944c47aebcefd4be68051f603ddd4
MinexAutomation <minex.automation@gmail.com>
2020-01-30 15:09:48 -0800

    Updated IServiceCollection extensions to use ServiceAction<T> functionality.

--- source/R5T.Oxford.Rivet.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
+++ source/R5T.Oxford.Rivet.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -2,16 +2,30 @@
 
 using Microsoft.Extensions.DependencyInjection;
 
+using R5T.Dacia;
+
 
 namespace R5T.Oxford.Rivet.Standard
 {
     public static class IServiceCollectionExtensions
     {
+        /// <summary>
+        /// Adds the Rivet-based <see cref="IOrganizationProvider"/> service.
+        /// </summary>
         public static IServiceCollection AddRivetOrganizationProvider(this IServiceCollection services)
         {
-            services.AddSingleton<IOrganizationProvider, RivetOrganizationProvider>();
+            services.AddDefaultRivetOrganizationProvider();
 
             return services;
+        }
+
+        /// <summary>
+        /// Adds the Rivet-based <see cref="IOrganizationProvider"/> service.
+        /// </summary>
+        public static ServiceAction<IOrganizationProvider> AddRivetOrganizationProviderAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<IOrganizationProvider>(() => services.AddRivetOrganizationProvider());
+            return serviceAction;
         }
     }
 }
